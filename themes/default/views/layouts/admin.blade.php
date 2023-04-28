ee06e5355ec3315730ef6becd62ff9d4959b571f
Almesaody <hameralmesaody@gmail.com>
2023-04-28 11:04:46 +0300

    Update

--- themes/default/views/layouts/admin.blade.php
+++ themes/default/views/layouts/admin.blade.php
@@ -1,27 +1,44 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="dark" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ 'Admin - ' . $title }}</title>
+    @isset($title)
+        <title>{{ config('app.name', 'Paymenter') . ' - ' . $title }}</title>
+    @else
+        <title>{{ config('app.name', 'Paymenter') }}</title>
+    @endisset
+
+    <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
     @vite(['themes/default/css/app.css', 'themes/default/js/app.js'], 'default')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+        if(!localStorage.theme) localStorage.theme = 'light'
+    </script>
+
 </head>
 
-<body class="font-sans antialiased">
-    <div id="app" class="min-h-screen bg-gray-100">
-        @include('layouts.adminnavigation')
+<body class="dark:bg-darkmode font-sans antialiased bg-gray-100">
+    <div id="app" class="dark:text-white min-h-screen dark:bg-darkmode">
+        @include('layouts.navigation')
+        <!-- Page Content -->
         <main>
             {{ $slot }}
         </main>
