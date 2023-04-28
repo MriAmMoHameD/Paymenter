19925f941375f44e83b6eb0b5d47f206077fbc9b
Almesaody <hameralmesaody@gmail.com>
2023-04-29 00:42:59 +0300

    Design Update Finished Updated admin navbar Started working on admin index

--- themes/default/views/layouts/admin.blade.php
+++ themes/default/views/layouts/admin.blade.php
@@ -6,39 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @isset($title)
-        <title>{{ config('app.name', 'Paymenter') . ' - ' . $title }}</title>
-    @else
-        <title>{{ config('app.name', 'Paymenter') }}</title>
-    @endisset
-
-    <!-- Fonts -->
+    <title>{{ 'Admin - ' . $title }}</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
     @vite(['themes/default/css/app.css', 'themes/default/js/app.js'], 'default')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
     <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
-    <script>
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia(
-                '(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-        }
-        if(!localStorage.theme) localStorage.theme = 'light'
-    </script>
-
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
-<body class="dark:bg-darkmode font-sans antialiased bg-gray-100">
-    <div id="app" class="dark:text-white min-h-screen dark:bg-darkmode">
-        @include('layouts.navigation')
-        <!-- Page Content -->
+<body class="font-sans antialiased">
+    <div id="app" class="dark:bg-darkmode min-h-screen bg-gray-100">
+        @include('layouts.adminnavigation')
         <main>
             {{ $slot }}
         </main>
